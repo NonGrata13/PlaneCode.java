666809dd67fb52ed326dc8be29daa8321bd2b1d7
NonGrata13 <andreyzasenko@gmail.com>
2022-12-15 23:13:48 +0200

    111

--- src/Package1/Plane.java
+++ src/Package1/Plane.java
@@ -3,17 +3,29 @@ package Package1;
 public class Plane {
     public static String model;
     public String passengers;
-    public Plane (String params){
-    model = params;
-}
-  public void setPassengers(String setPassengers){
+
+    public Plane(String params) {
+        model = params;
+    }
+    public void setPassengers(String setPassengers) {
         passengers = setPassengers;
     }
 
-    public boolean isPassengerOnPlane (){
-    return true;
+    private String[] passengersNames = new String[6];
+
+    public String[] getPassengersNames() {
+        return passengersNames;
+    }
+    public void setPassengersNames(String[] passengersNames) {
+        this.passengersNames = passengersNames;
+    }
+    public String getPassenger(int index) {
+        return passengersNames[index];
     }
 
-}
+    //  public boolean isPassengerOnPlane () {
+   //     return true;}
 
+
+}
 
--- src/Package1/PlaneCode.java
+++ src/Package1/PlaneCode.java
@@ -1,31 +1,55 @@
 package Package1;
 
 public class PlaneCode {
-    public static void main (String... args){
-Plane thisPlane = new Plane("Boeing747");
-System.out.println("This plane model is " + thisPlane.model);
-thisPlane.setPassengers("300");
-System.out.println("Number of passengers for this plane is " + thisPlane.passengers);
+    public static void main (String... args) {
+            Plane thisPlane = new Plane("Boeing747");
+            System.out.println("This plane model is " + thisPlane.model);
+            thisPlane.setPassengers("300");
+            System.out.println("Number of passengers for this plane is " + thisPlane.passengers);
 
-System.out.println("________________________________________________________________________________________________");
+            System.out.println("________________________________________________________________________________________________");
 
-Plane thatPlane = new Plane("F11");
-System.out.println("That plane model is " + thatPlane.model);
-thatPlane.setPassengers("0");
-System.out.println("Number of passengers for that plane is " + thatPlane.passengers + ", because it is fight get");
+            Plane thatPlane = new Plane("F11");
+            System.out.println("That plane model is " + thatPlane.model);
+            thatPlane.setPassengers("0");
+            System.out.println("Number of passengers for that plane is " + thatPlane.passengers + ", because it is fight get");
 
-System.out.println("________________________________________________________________________________________________");
-        Plane lastPlane = new Plane("Boeing777");
-        System.out.println("This plane model is " + lastPlane.model);
+            System.out.println("________________________________________________________________________________________________");
+            Plane lastPlane = new Plane("Boeing777");
+            System.out.println("This plane model is " + lastPlane.model);
+            thatPlane.setPassengers("6");
+            System.out.println("Number of passengers for last plane is " + lastPlane.passengers + ", because it's VIP Aircraft");
 
-        final String[] passengers = new String[6];
-        passengers [0] = "Rich Dude";
-        passengers [1] = "Some Dude";
-        passengers [2] = "Any Dude";
-        passengers [3] = "This Dude";
-        passengers [4] = "That Dude";
-        passengers [5] = "Last Dude";
+            String[] richPassengers = new String[6];
+            richPassengers[0] = "Rich Dude";
+            richPassengers[1] = "Some Dude";
+            richPassengers[2] = "Any Dude";
+            richPassengers[3] = "This Dude";
+            richPassengers[4] = "That Dude";
+            richPassengers[5] = "Last Dude";
+            lastPlane.setPassengersNames(richPassengers);
+
+            for (int i = 0; i < lastPlane.getPassengersNames().length; i++) { // какая разница, если эффект тот же?
+            System.out.println(lastPlane.getPassengersNames()[i]);}
+            System.out.println("________________________________________________________________________________________________");
+
+            for (int i = 0; i < lastPlane.getPassengersNames().length; i++) {
+                     System.out.println(lastPlane.getPassenger(i)); // можно закомментить строку, чтобы чисто свич запустился
+                    switch (lastPlane.getPassenger(i)){
+                            case "Rich Dude":
+                                    System.out.println("This is Rich Dude");
+                                    break;
+                            case "Some Dude":
+                                    System.out.println("This is Some Dude");
+                                    break;
+                            case "Last Dude":
+                                    System.out.println("This is Last Dude");
+                                    break;
+                            default: System.out.println("There are no dudes on plane");
+                    }
+            }
 
 
-    }
-}
+            }
+
+            }
